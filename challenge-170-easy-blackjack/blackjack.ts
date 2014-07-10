107fbd2a63c05818796fa2b382163754cdd8272a
brian_d <bdahl@daptiv.com>
2014-07-10 14:47:10 -0700

    Card parsing finished

--- challenge-170-easy-blackjack/blackjack.ts
+++ challenge-170-easy-blackjack/blackjack.ts
@@ -2,12 +2,11 @@ declare function require(name:string);
 
 var fs = require('fs');
 
-enum Cards { One = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
+enum Card { One = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
 
 class Player {
     name:string;
-    cards:Array<Cards>;
-
+    cards:Array<Card>;
 }
 
 function processFile(err, data) {
@@ -31,22 +30,34 @@ function processFile(err, data) {
 function parsePlayer(line:string) {
     var player = new Player();
 
+    // Maybe replace with line.substring(0, line.indexOf(' ') or something
     var nameSplit:Array<string> = line.split(':');
 
     player.name = nameSplit[0];
 
+    var cardsSplit:Array<string> = nameSplit[1].split(',');
+
+    player.cards = [];
+    for(var i:number = 0; i < cardsSplit.length; i++) {
+        player.cards.push(parseCard(cardsSplit[i]));
+    }
+
     return player;
+}
+
+function parseCard(cardText:string) {
+    var cardName = cardText.trim();
+    cardName = cardName.substring(0, cardName.indexOf(" "));
+    var cardValue : Card = Card[cardName];
+    return cardValue;
 }
 
 function checkGame(players:Array<Player>) {
     console.log("Player Count: " + players.length);
 
     for(var i:number = 0; i < players.length; i++) {
-        console.log("Player " + (i + 1) + ": " + players[i].name);
+        console.log("Player " + (i + 1) + ": " + players[i].name + " - " + players[i].cards);
     }
-
-
-
 };
 
 function openFile(filename:string) {
